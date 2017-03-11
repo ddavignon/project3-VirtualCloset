27bb511214016e4fba1f58f8a07ab55720148666
Dustin D'Avignon <dustin@Dustins-MacBook-Pro.local>
2017-03-11 12:07:23 -0800

    general setup, app file created

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import reducers from './reducers';
+// import { Header } from './components/common';
+
+/*class App extends Component {
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Header headerText="Tech Stack" />
+                </View>
+            </Provider>
+        );
+    }
+}*/
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Hello App!
+                </Text>
+            </View>
+        );
+    }
+}
+export default App;
