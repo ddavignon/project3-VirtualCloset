10e0657f8f165c4b1063e74348a49f1a81525eb5
Dustin D'Avignon <dustin@Dustins-MacBook-Pro.local>
2017-03-11 12:18:07 -0800

    inital redux setup, add reducer

--- src/app.js
+++ src/app.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-// import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
-// import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import { Header } from './components/common';
-
-/*class App extends Component {
-    render() {
-        return (
-            <Provider store={createStore(reducers)}>
-                <View>
-                    <Header headerText="Tech Stack" />
-                </View>
-            </Provider>
-        );
-    }
-}*/
 
 class App extends Component {
     render() {
         return (
-            <View>
-                <Header headerText="My Closet" />
-                <Text>
-                    Hello App!
-                </Text>
-            </View>
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Header headerText="My Closet" />
+                    <Text>
+                        Hello App!
+                    </Text>
+                </View>
+            </Provider>
         );
     }
 }
+
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    var_placeholder: () => []
+});
