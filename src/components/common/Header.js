bc868d206d9b1c51bcd599cf561420e316fbbf15
Dustin D'Avignon <dustin@Dustins-MacBook-Pro.local>
2017-03-11 12:14:05 -0800

    add color to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -13,7 +13,7 @@ const Header = (props) => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8',
+        backgroundColor: 'rgb(0,188,212)',
         justifyContent: 'center',
         alignItems: 'center',
         height: 60,
@@ -25,7 +25,8 @@ const styles = {
         position: 'relative'
     },
     textStyle: {
-        fontSize: 20
+        fontSize: 20,
+        color: '#FFF'
     }
 };
 
