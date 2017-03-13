286c08cef0600306b6eeaf2c3b4b75132295e584
Dustin D'Avignon <dustin@Dustins-MacBook-Pro.local>
2017-03-12 21:27:37 -0700

    add router and setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import { Header } from './components/common';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
     componentWillMount() {
@@ -25,10 +23,7 @@ class App extends Component {
         const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
             <Provider store={store}>
-                <View>
-                    <Header headerText="My Closet" />
-                    <LoginForm />
-                </View>
+                <Router />
             </Provider>
         );
     }
