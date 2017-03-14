b4650b0b25ab899a1e67e379d3527d3217b6fe68
Dustin D'Avignon <dustin@Dustins-MacBook-Pro.local>
2017-03-14 11:36:06 -0700

    ES6 refactor

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   PixelRatio,
-  TouchableOpacity,
   Image,
 } from 'react-native';
 
 import ImagePicker from 'react-native-image-picker';
 
-class ImageUpload extends React.Component {
+class ImageUpload extends Component {
 
   state = {
     avatarSource: null
@@ -19,9 +18,6 @@ class ImageUpload extends React.Component {
   componentWillMount() {
         const options = {
         title: 'Select Avatar',
-        // customButtons: [
-        //     {name: 'fb', title: 'Choose Photo from Facebook'},
-        // ],
         storageOptions: {
             skipBackup: true,
             path: 'images'
@@ -35,8 +31,6 @@ class ImageUpload extends React.Component {
         console.log('User cancelled photo picker');
       } else if (response.error) {
         console.log('ImagePicker Error: ', response.error);
-      // } else if (response.customButton) {
-      //   console.log('User tapped custom button: ', response.customButton);
       } else {
         const source = { uri: response.uri };
 
@@ -50,58 +44,16 @@ class ImageUpload extends React.Component {
     });
   }
 
-  selectPhotoTapped() {
-    // const options = {
-    //   quality: 1.0,
-    //   maxWidth: 500,
-    //   maxHeight: 500,
-    //   storageOptions: {
-    //     skipBackup: true
-    //   }
-    // };
-    // const options = {
-    //     title: 'Select Avatar',
-    //     // customButtons: [
-    //     //     {name: 'fb', title: 'Choose Photo from Facebook'},
-    //     // ],
-    //     storageOptions: {
-    //         skipBackup: true,
-    //         path: 'images'
-    //     }
-    // };
-
-    // ImagePicker.showImagePicker(options, (response) => {
-    //   console.log('Response = ', response);
-
-    //   if (response.didCancel) {
-    //     console.log('User cancelled photo picker');
-    //   } else if (response.error) {
-    //     console.log('ImagePicker Error: ', response.error);
-    //   // } else if (response.customButton) {
-    //   //   console.log('User tapped custom button: ', response.customButton);
-    //   } else {
-    //     const source = { uri: response.uri };
-
-    //     // You can also display the image using data:
-    //     // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-    //     this.setState({
-    //       avatarSource: source
-    //     });
-    //   }
-    // });
-  }
-
   render() {
+    const { container, avatar, avatarContainer } = styles;
+    
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
-          <View style={[styles.avatar, styles.avatarContainer, { marginBottom: 20 }]}>
+      <View style={container}>
+          <View style={[avatar, avatarContainer, { marginBottom: 20 }]}>
           { this.state.avatarSource === null ? <Text>Select a Photo</Text> :
-            <Image style={styles.avatar} source={this.state.avatarSource} />
+            <Image style={avatar} source={this.state.avatarSource} />
           }
           </View>
-        </TouchableOpacity>
       </View>
     );
   }
