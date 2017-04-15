642ad225b3f5cf22e40af233917097421ca368d8
brandanlockwood <blockwood@csumb.edu>
2017-04-15 04:52:40 +0000

    checking for the command

--- server/resources/AvatarAI.py
+++ server/resources/AvatarAI.py
@@ -11,25 +11,29 @@ from models.closet import ClosetModel
 
 class avatar(Resource):
     @jwt_required()
-    def post(self):
-        today = None
-        # weatehr is assumed to be warm as of now if any errors
-        dark_sky_key = os.getenv("DARK_SKY_KEY")
-        get_style = 'warm'
-        try:
-            lat = request.args.get('lat')
-            lng = request.args.get('lng')
-            forecast = forecastio.load_forecast(dark_sky_key, lat, lng)
-            today=forecast.hourly().data[0].apparentTemperature
-            print today
-            if today < 65:
-                get_style = 'cold'
-        except Exception as e:
-            print 'Could not fetch weather', e
+    def get(self):
+        command =request.args.get("command")
+        if "recommend" in command:
+            
+            
+            today = None
+            # weatehr is assumed to be warm as of now if any errors
+            dark_sky_key = os.getenv("DARK_SKY_KEY")
+            get_style = 'warm'
+            try:
+                lat = request.args.get('lat')
+                lng = request.args.get('lng')
+                forecast = forecastio.load_forecast(dark_sky_key, lat, lng)
+                today=forecast.hourly().data[0].apparentTemperature
+                print today
+                if today < 65:
+                    get_style = 'cold'
+            except Exception as e:
+                print 'Could not fetch weather', e
 
-        closet = ClosetModel.find_by_uid(current_identity.id).json()
-        print get_style
-        return {
+            closet = ClosetModel.find_by_uid(current_identity.id).json()
+            print get_style
+            return {
             'weather':today,
             'shirts': [item.json() for item in ItemModel.query.filter_by(style=get_style).filter_by(type_clothing='shirt').filter_by(closet_id=closet['_id']).all()],
             'pants': [item.json() for item in ItemModel.query.filter_by(style=get_style).filter_by(type_clothing='pants').filter_by(closet_id=closet['_id']).all()],
@@ -38,8 +42,11 @@ class avatar(Resource):
             'outerwear': [item.json() for item in ItemModel.query.filter_by(style=get_style).filter_by(type_clothing='outerwear').filter_by(closet_id=closet['_id']).all()],
             'text': 'Here is what I found'
         }
+        return 401
+        
+        
     @jwt_required()
-    def get(self):
+    def post(self):
         return send_file('static/avatarAnimate.gif', mimetype='image/gif')
 
         
