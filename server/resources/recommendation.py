a91e222ac27f985f4209966de433a4d204fe70ab
brandanlockwood <blockwood@csumb.edu>
2017-04-17 06:37:37 +0000

    amazon

--- server/resources/recommendation.py
+++ server/resources/recommendation.py
@@ -3,6 +3,7 @@ from flask_jwt import jwt_required, current_identity
 from flask import request,render_template
 from werkzeug.utils import secure_filename
 
+import random
 import json, forecastio, os, boto3, random, string
 
 
@@ -13,6 +14,11 @@ class recommendClothes(Resource):
     @jwt_required()
     def get(self):
         info =[closet.json() for closet in ClosetModel.query.filter_by(user_id=current_identity.id).all()]
-        
-        return info
+        items = info[0]["items"]
+        if items.length > 0:
+            item =random.choice(random.choice(items))
+            return render_template('recommendations.html',search=item["description"])
+            
+        return return render_template('recommendations.html',search='shirt')
+       
     
