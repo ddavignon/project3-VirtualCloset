5e9aa00ad9253ccc35ab64aa13aba94cd61970c2
brandanlockwood <blockwood@csumb.edu>
2017-04-21 17:59:38 +0000

    new endpoint get request moving head

--- server/resources/AvatarAI.py
+++ server/resources/AvatarAI.py
@@ -44,9 +44,14 @@ class avatar(Resource):
         }
         return 401
         
-        
-    @jwt_required()
-    def post(self):
+        @jwt_required()
+        def post(self):
         return send_file('static/VirtualClosetAvatar.gif', mimetype='image/gif')
+    
 
         
+class movingAvatar:
+    @jwt_required()
+    def get(self):
+        return send_file('static/VirtualClosetAvatar.gif', mimetype='image/gif')
+    
